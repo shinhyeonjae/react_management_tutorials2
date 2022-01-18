1f0af4659ae6e09e14cdb136796344cbc616e36b
shin hyeon jae <bbbb817@naver.com>
2022-01-19 01:24:10 +0900

    Create React Procject

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="gray_background">
+      <img src={logo} lat = "logo"/>
+      <h2>let's develop management system!!</h2>
+    </div>
+  );
+}
+
+export default App;
